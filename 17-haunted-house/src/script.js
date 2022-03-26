4b62484d6c08a7be256de8dd2e70f2dd756f5de2
Duane Cilliers <hello@duane.co.za>
2022-03-26 10:51:39 +0200

    exercise 17: add door

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -52,6 +52,15 @@ floor.rotation.x = - Math.PI * 0.5
 floor.position.y = 0
 scene.add(floor)
 
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(2, 2),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
 /**
  * Lights
  */
