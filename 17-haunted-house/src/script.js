bece6badde414e45ea9c80085794accd14d8417d
Duane Cilliers <hello@duane.co.za>
2022-03-26 11:29:24 +0200

    exercise 17: add walls texture

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -26,11 +26,16 @@ const textureLoader = new THREE.TextureLoader()
 
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientocclusion.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
 const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+const bricksColorTexture = textureLoader.load('/textures/bricks/color.jpg')
+const bricksAmbientOcclusionTexture = textureLoader.load('/textures/bricks/ambientOcclusion.jpg')
+const bricksNormalTexture = textureLoader.load('/textures/bricks/normal.jpg')
+const bricksRoughnessTexture = textureLoader.load('/textures/bricks/roughness.jpg')
 
 /**
  * House
@@ -41,8 +46,14 @@ scene.add(house)
 // walls
 const walls = new THREE.Mesh(
     new THREE.BoxBufferGeometry(4, 2.5, 4),
-    new THREE.MeshStandardMaterial({ color: '#ac8e82'})
+    new THREE.MeshStandardMaterial({
+        map: bricksColorTexture,
+        aoMap: bricksAmbientOcclusionTexture,
+        normalMap: bricksNormalTexture,
+        roughnessMap: bricksRoughnessTexture
+    })
 )
+walls.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(walls.geometry.attributes.uv.array, 2))
 walls.position.y = 2.5 / 2
 house.add(walls)
 
