3499d72268a1ba27e4e62eb07137a24591e863d4
Duane Cilliers <hello@duane.co.za>
2022-03-26 11:18:42 +0200

    exercise 17: add fog

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -15,6 +15,10 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Fog
+const fog = new THREE.Fog('#262837', 1, 15)
+scene.fog = fog
+
 /**
  * Textures
  */
@@ -169,6 +173,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837')
 
 /**
  * Animate
