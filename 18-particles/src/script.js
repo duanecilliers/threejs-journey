10e82539638f3da11901d413746761df26f8c547
Duane Cilliers <hello@duane.co.za>
2022-03-26 12:41:58 +0200

    exercise 18: animate particles with bad practice

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -111,6 +111,17 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update particles
+    // particles.rotation.y = elapsedTime * 0.2
+    for (let i = 0; i < count; i++) {
+        const i3 = i * 3
+        const x = particlesGeometry.attributes.position.array[i3]
+        
+        // this leads to performance issues when animating large arrays. Use a custom shader
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
+
     // Update controls
     controls.update()
 
