37eb6f6d356ad041034d037eeebf16949a9901df
Duane Cilliers <hello@duane.co.za>
2022-03-26 12:33:41 +0200

    exercise 18: random colors

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -35,17 +35,25 @@ const particlesMaterial = new THREE.PointsMaterial({
     // depthTest: false,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
-    color: '#ff88cc'
+    // color: '#ff88cc',
+    vertexColors: true
 })
 const count = 5000
 const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 for (let i = 0; i < count * 3; i++) {
     positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
 }
 particlesGeometry.setAttribute(
     'position',
     new THREE.BufferAttribute(positions, 3)
 )
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
