8f026610686aa030d1f6b3242bb87bfe21bdca53
Duane Cilliers <hello@duane.co.za>
2022-05-26 20:09:43 +0200

    execise 23: add gltf loader and log

--- 23-imported-models/src/script.js
+++ 23-imported-models/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as dat from 'lil-gui'
 
 /**
@@ -14,6 +15,17 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+gltfLoader.load(
+    '/models/Duck/glTF/Duck.gltf',
+    (gltf) => {
+        console.log(gltf)
+    }
+)
 
 /**
  * Floor
