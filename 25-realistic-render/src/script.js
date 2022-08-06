2d3e4aae45431ecc62a0a902be7b3c934b7dd1a4
Duane Cilliers <hello@duane.co.za>
2022-08-06 10:50:48 +0200

    enable antialiasing on the renderer instantiation

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -118,7 +118,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
