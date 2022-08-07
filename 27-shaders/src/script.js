89028fbeb05d76b5fd190217ed671fe30dc06a00
Duane Cilliers <hello@duane.co.za>
2022-08-07 11:40:14 +0200

    apply color with uniforms

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -44,6 +44,7 @@ const material = new THREE.RawShaderMaterial({
     uniforms: {
         uFrequency: { value: new THREE.Vector2(10, 5) },
         uTime: { value: 0 },
+        uColor: { value: new THREE.Color('orange') }
     },
     // wireframe: true,
     side: THREE.DoubleSide,
