7f35ac9709c7f0b7acb84eab19dae81b852caf51
Duane Cilliers <hello@duane.co.za>
2022-08-07 12:59:04 +0200

    add pattern 10 - flip pattern 9 to x axis

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -22,7 +22,11 @@ void main()
     // strength = step(0.5, strength);
 
     // Pattern 9
-    float strength = mod(vUv.y * 10.0, 1.0);
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.8, strength);
+
+    // Pattern 10
+    float strength = mod(vUv.x * 10.0, 1.0);
     strength = step(0.8, strength);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
