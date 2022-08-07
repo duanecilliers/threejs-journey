00c0e3f6dc8d97433f70ac6d64e0a574cd699995
Duane Cilliers <hello@duane.co.za>
2022-08-07 15:26:37 +0200

    add water shader boilerplate

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import waterVertexShader from './shaders/water/vertex.glsl'
+import waterFragmentShader from './shaders/water/fragment.glsl'
 
 /**
  * Base
@@ -22,7 +24,10 @@ const scene = new THREE.Scene()
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
 // Material
-const waterMaterial = new THREE.MeshBasicMaterial()
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+})
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
