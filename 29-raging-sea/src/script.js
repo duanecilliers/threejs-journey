7caec4910eda5bef5487fa35486e6f6517630bb7
Duane Cilliers <hello@duane.co.za>
2022-08-07 15:43:45 +0200

    add waves speed

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -31,12 +31,14 @@ const waterMaterial = new THREE.ShaderMaterial({
         uTime: { value: 0 },
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+        uBigWavesSpeed: { value: 0.75 }
     }
 })
 
 gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value', 0, 1).step(0.001).name('uBigWavesElevation')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequency X')
 gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(10).step(0.001).name('uBigWavesFrequency Y')
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value', 0, 1).step(0.001).name('uBigWavesSpeed')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
--- 29-raging-sea/src/shaders/water/vertex.glsl
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -1,12 +1,13 @@
 uniform float uTime;
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime) *
-                    sin(modelPosition.z * uBigWavesFrequency.y + uTime) *
+  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                    sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
                     uBigWavesElevation;
 
   modelPosition.y += elevation;
