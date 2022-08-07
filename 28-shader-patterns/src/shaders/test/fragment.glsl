d166d5d7ab369490488b3a2464d24d1df5bcfbf8
Duane Cilliers <hello@duane.co.za>
2022-08-07 14:25:39 +0200

    add pattern 44

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -211,11 +211,17 @@ void main()
     // float strength = angle;
 
     // Pattern 43
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // angle = mod(angle *= 20.0, 1.0);
+    // float strength = angle;
+
+    // Pattern 44
     float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
     angle /= PI * 2.0;
     angle += 0.5;
-    angle = mod(angle *= 20.0, 1.0);
-    float strength = angle;
+    float strength = sin(angle * 50.0);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
