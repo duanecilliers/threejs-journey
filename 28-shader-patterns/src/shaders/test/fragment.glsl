c64fdd7da18f04f189b33ebae483cb9d6940c51b
Duane Cilliers <hello@duane.co.za>
2022-08-07 12:46:45 +0200

    update pattern 3 code

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -2,5 +2,8 @@ varying vec2 vUv;
 
 void main()
 {
-    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
+    // Pattern 3
+    float strength = vUv.x;
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
