984f2af082539bdeb27908910605648d1d3cd6a9
Duane Cilliers <hello@duane.co.za>
2022-08-07 12:47:32 +0200

    add pattern 4 - vertical gray scale

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -3,7 +3,10 @@ varying vec2 vUv;
 void main()
 {
     // Pattern 3
-    float strength = vUv.x;
+    // float strength = vUv.x;
+
+    // Pattern 4
+    float strength = vUv.y;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
