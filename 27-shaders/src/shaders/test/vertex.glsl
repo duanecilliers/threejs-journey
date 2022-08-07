37849725a4d8ea21832faeadc8eddb99235acce3
Duane Cilliers <hello@duane.co.za>
2022-08-07 11:29:02 +0200

    reset to wave for uniforms test

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -9,8 +9,8 @@ varying float vRandom;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
-  modelPosition.z += aRandom * 0.1;
+  modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+  // modelPosition.z += aRandom * 0.1;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
