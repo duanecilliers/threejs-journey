21a9a71aa26d9c842714fd5122865b068fc4c8bf
Duane Cilliers <hello@duane.co.za>
2022-08-07 12:58:30 +0200

    add pattern 9 - tweak step parameter

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -18,8 +18,12 @@ void main()
     // float strength = mod(vUv.y * 10.0, 1.0);
 
     // Pattern 8
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // strength = step(0.5, strength);
+
+    // Pattern 9
     float strength = mod(vUv.y * 10.0, 1.0);
-    strength = step(0.5, strength);
+    strength = step(0.8, strength);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
