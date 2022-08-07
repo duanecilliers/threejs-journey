5624f6165479879807c2407aeda4c2ed56db10c4
Duane Cilliers <hello@duane.co.za>
2022-08-07 13:38:49 +0200

    add exercise 22

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -79,7 +79,11 @@ void main()
     // );
 
     // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+
+    // Pattern 22
     float strength = floor(vUv.x * 10.0) / 10.0;
+    strength *= floor(vUv.y * 10.0) / 10.0;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
