16b01ecb75201001b4f480283a4e0c3d2ffd2243
Duane Cilliers <hello@duane.co.za>
2022-08-07 13:47:11 +0200

    add pattern 26

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -100,11 +100,14 @@ void main()
     // float strength = random(gridUv);
 
     // Pattern 25
-    vec2 gridUv = vec2(
-        floor(vUv.x * 10.0) / 10.0,
-        floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0
-    );
-    float strength = random(gridUv);
+    // vec2 gridUv = vec2(
+    //     floor(vUv.x * 10.0) / 10.0,
+    //     floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0
+    // );
+    // float strength = random(gridUv);
+
+    // Pattern 26
+    float strength = length(vUv);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
