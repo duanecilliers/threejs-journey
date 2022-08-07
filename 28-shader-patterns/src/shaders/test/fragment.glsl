5e87a921175a8c9813b94266437ea6b9200023b6
Duane Cilliers <hello@duane.co.za>
2022-08-07 14:35:02 +0200

    add pattern 47

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -275,7 +275,10 @@ void main()
     // float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
 
     // Pattern 46
-    float strength = cnoise(vUv * 10.0);
+    // float strength = cnoise(vUv * 10.0);
+
+    // Pattern 47
+    float strength = step(0.0, cnoise(vUv * 10.0));
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
